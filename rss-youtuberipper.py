6e49d66833d3821bd2caa73c3e795630094b90cf
flinkeandreas <35853412+flinkeandreas@users.noreply.github.com>
2018-01-26 23:43:55 +0100

    Update rss-youtuberipper.py

--- rss-youtuberipper.py
+++ rss-youtuberipper.py
@@ -2,9 +2,9 @@ import requests # handle the http requests to the website
 import feedparser # parse the rssfeeds
 from pytube import YouTube # work the youtube magic
 
-channel_urls = ["https://www.youtube.com/feeds/videos.xml?channel_id=UCWRZyxG8M420d0Bxj7DtLbw", # Medina (danish pop)
-                "https://www.youtube.com/feeds/videos.xml?channel_id=UCyqhj3unXdUW89Jgv2Lgd7A", # Linda Pira (swedish hiphop)
-	              "https://www.youtube.com/feeds/videos.xml?channel_id=UCM4tt6pjbeSRL4pl-EUm0Hg"] # Cherrie (more swedish hiphop)
+channel_urls = ["https://www.youtube.com/feeds/videos.xml?channel_id=UCWRZyxG8M420d0Bxj7DtLbw", # Medina
+                "https://www.youtube.com/feeds/videos.xml?channel_id=UCyqhj3unXdUW89Jgv2Lgd7A", # Linda Pira
+                "https://www.youtube.com/feeds/videos.xml?channel_id=UCM4tt6pjbeSRL4pl-EUm0Hg"] # Cherrie
 
 for url in channel_urls:
 	rssrawdata = feedparser.parse(url)
